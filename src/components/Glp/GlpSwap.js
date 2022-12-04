1a05c7cf88b7c307a998fd3d3272df1c7db9cbdb
Tam Nguyen <nntam2013@gmail.com>
2022-12-04 16:30:51 +0700

    fix: updagte fee glp tracker

--- src/components/Glp/GlpSwap.js
+++ src/components/Glp/GlpSwap.js
@@ -563,7 +563,7 @@ export default function GlpSwap(props) {
   const buyGlp = () => {
     setIsSubmitting(true);
 
-    const minGlp = glpAmount.mul(BASIS_POINTS_DIVISOR - 1100).div(BASIS_POINTS_DIVISOR);
+    const minGlp = glpAmount.mul(BASIS_POINTS_DIVISOR - savedSlippageAmount).div(BASIS_POINTS_DIVISOR);
 
     const contract = new ethers.Contract(rewardRouterAddress, RewardRouter.abi, library.getSigner());
     const method = swapTokenAddress === AddressZero ? "mintAndStakeGlpETH" : "mintAndStakeGlp";
