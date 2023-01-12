80174c300be7e0800e544d1d9242f2366396ede6
phuongnn <phuongnn>
2023-01-12 16:11:01 +0700

    change subgraphs url

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -140,12 +140,12 @@ export const BSC_RPC_PROVIDERS = [
 ];
 
 export const TESTNET_RPC_PROVIDERS = [
+  "https://data-seed-prebsc-2-s3.binance.org:8545",
   "https://data-seed-prebsc-1-s1.binance.org:8545",
   "https://data-seed-prebsc-2-s1.binance.org:8545",
   "https://data-seed-prebsc-1-s2.binance.org:8545",
   "https://data-seed-prebsc-2-s2.binance.org:8545",
   "https://data-seed-prebsc-1-s3.binance.org:8545",
-  "https://data-seed-prebsc-2-s3.binance.org:8545",
 ];
 
 export const RPC_PROVIDERS = {
