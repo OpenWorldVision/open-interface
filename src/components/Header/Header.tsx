f928dfb0a9f41f47bb24efbc961a65a1901191bb
dang.duonghai <dang.duonghai@digipaysolution.com>
2023-01-13 23:43:57 +0700

    update mobile UI

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -117,8 +117,8 @@ export function Header({
           >
             <div className="App-header-container-left">
               <div className="App-header-menu-icon-block" onClick={() => setIsDrawerVisible(!isDrawerVisible)}>
-                {!isDrawerVisible && <RiMenuLine className="App-header-menu-icon" color={"#375BD2"} />}
-                {isDrawerVisible && <FaTimes className="App-header-menu-icon" color={"#375BD2"} />}
+                {!isDrawerVisible && <RiMenuLine className="App-header-menu-icon" />}
+                {isDrawerVisible && <FaTimes className="App-header-menu-icon" />}
               </div>
               <div className="App-header-link-main clickable" onClick={() => setIsDrawerVisible(!isDrawerVisible)}>
                 <img src={logoImg} className="App-header-logo" alt="OPEN Logo" />
