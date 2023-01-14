2833091957e7b5d4f3a093014876de0809b818ae
phuongnn <phuongnn>
2023-01-15 00:05:44 +0700

    revert code

--- src/config/subgraph.ts
+++ src/config/subgraph.ts
@@ -1,4 +1,4 @@
-import { ARBITRUM, AVALANCHE, TESTNET } from "./chains";
+import { ARBITRUM, AVALANCHE, MAINNET, TESTNET } from "./chains";
 
 export const SUBGRAPH_URLS = {
   [ARBITRUM]: {
@@ -16,7 +16,11 @@ export const SUBGRAPH_URLS = {
     chainLink: "https://api.thegraph.com/subgraphs/name/deividask/chainlink",
   },
   [TESTNET]: {
-    stats: "https://graphiql.openworld.vision/subgraphs/name/nnphuong1997/open-bsc-stats2/graphql",
-    referrals: "https://api.thegraph.com/subgraphs/name/nnphuong9718/open-bsc-referrals-mainnet/",
+    stats: "https://graphiql.openworld.vision/subgraphs/name/nnphuong1997/open-bsc-stats",
+    referrals: "https://graphiql.openworld.vision/subgraphs/name/nnphuong1997/open-bsc-referrals8",
+  },
+  [MAINNET]: {
+    stats: "https://graphiql.openworld.vision/subgraphs/name/nnphuong1997/open-bsc-stats",
+    referrals: "https://api.thegraph.com/subgraphs/name/nnphuong9718/open-bsc-referrals-mainnet",
   },
 };
--- src/lib/subgraph/clients.ts
+++ src/lib/subgraph/clients.ts
@@ -1,6 +1,6 @@
 import { createClient } from "./utils";
 import { SUBGRAPH_URLS } from "config/subgraph";
-import { ARBITRUM, ARBITRUM_TESTNET, AVALANCHE, TESTNET } from "config/chains";
+import { ARBITRUM, ARBITRUM_TESTNET, AVALANCHE, MAINNET, TESTNET } from "config/chains";
 
 export const chainlinkClient = createClient(SUBGRAPH_URLS.common.chainLink);
 
@@ -10,7 +10,8 @@ export const nissohGraphClient = createClient(SUBGRAPH_URLS[ARBITRUM].nissohVaul
 
 export const avalancheGraphClient = createClient(SUBGRAPH_URLS[AVALANCHE].stats);
 export const avalancheReferralsGraphClient = createClient(SUBGRAPH_URLS[AVALANCHE].referrals);
-export const bscReferralsGraphClient = createClient(SUBGRAPH_URLS[TESTNET].referrals);
+export const bscTestnetReferralsGraphClient = createClient(SUBGRAPH_URLS[TESTNET].referrals);
+export const bscReferralsGraphClient = createClient(SUBGRAPH_URLS[MAINNET].referrals);
 
 export function getGmxGraphClient(chainId: number) {
   if (chainId === ARBITRUM) {
