389f7312dca251aa24caab9e390bbbd7d42db0c7
phuongnn <phuongnn>
2023-01-14 17:24:33 +0700

    update subgraph prod

--- src/config/subgraph.ts
+++ src/config/subgraph.ts
@@ -17,7 +17,7 @@ export const SUBGRAPH_URLS = {
     owChainLink: "https://api.thegraph.com/subgraphs/name/nntam2013/chainlink_pricefeed_bnb_2",
   },
   [TESTNET]: {
-    stats: "https://graphiql.openworld.vision/subgraphs/name/nnphuong1997/open-bsc-stats",
-    referrals: "https://graphiql.openworld.vision/subgraphs/name/nnphuong1997/open-bsc-referrals6",
+    stats: "https://graphiql.openworld.vision/subgraphs/name/nnphuong1997/open-bsc-stats2/graphql",
+    referrals: "https://api.thegraph.com/subgraphs/name/nnphuong9718/open-bsc-referrals-mainnet/",
   },
 };
