250d27cf24dd2144835e07829e7f28182b9ebf48
dang.duonghai <dang.duonghai@digipaysolution.com>
2023-03-31 22:51:16 +0700

    feat: add external link

--- src/pages/ReferralTerms/ReferralTerms.js
+++ src/pages/ReferralTerms/ReferralTerms.js
@@ -3,6 +3,7 @@ import Footer from "components/Footer/Footer";
 import SEO from "components/Common/SEO";
 import { getPageTitle } from "lib/legacy";
 import "./ReferralTerms.css";
+import ExternalLink from "components/ExternalLink/ExternalLink";
 
 export default function ReferralTerms() {
   return (
@@ -17,13 +18,15 @@ export default function ReferralTerms() {
           <div className="content">
             <div className="section">
               <p className="body-text">
-                Welcome to OPEN.io ("we," "us," or "our"), the informational resource for OPEN Protocol, as defined
-                below.
+                Welcome to <ExternalLink href="https://openworld.vision/">OPEN</ExternalLink> ("we," "us," or "our"),
+                the informational resource for OPEN Protocol, as defined below.
               </p>
               <p className="body-text">
-                OPEN.io provides information and resources about the fundamentals of the decentralized non-custodial
-                protocol called the OPEN Protocol (the "OPEN Protocol," "Protocol," or "OPEN DApp"). OPEN.io is not an
-                available access point to the OPEN Protocol.
+                <ExternalLink href="https://openworld.vision/">OPEN</ExternalLink> provides information and resources
+                about the fundamentals of the decentralized non-custodial protocol called the OPEN Protocol (the "OPEN
+                Protocol," "Protocol," or "OPEN DApp").{" "}
+                <ExternalLink href="https://openworld.vision/">OPEN</ExternalLink> is not an available access point to
+                the OPEN Protocol.
               </p>
               <p className="body-text">
                 These Terms and Conditions and any other documents incorporated herein by reference (collectively, these
@@ -62,9 +65,10 @@ export default function ReferralTerms() {
                 someone about the OPEN Protocol.
               </p>
               <p className="body-text">
-                2.2 OPEN.io is not responsible for (i) lost sales or lost opportunity to earn Rebates due to any cause,
-                such as technical difficulties or over-capacity, including system overload in the BSC blockchains; (ii)
-                tracking Rebates, Discounts, or any other data, as this is handled by independent, smart contracts.
+                2.2 <ExternalLink href="https://openworld.vision/">OPEN</ExternalLink> is not responsible for (i) lost
+                sales or lost opportunity to earn Rebates due to any cause, such as technical difficulties or
+                over-capacity, including system overload in the BSC blockchains; (ii) tracking Rebates, Discounts, or
+                any other data, as this is handled by independent, smart contracts.
               </p>
               <p className="body-text">
                 2.3 You will be excluded from the Referral Program, following the directives of the OPEN tokenholders:
@@ -82,8 +86,9 @@ export default function ReferralTerms() {
                 will never be retroactive.
               </p>
               <p className="body-text">
-                OPEN.io is under no obligation for Rebates or Discounts to any Referrer or Trader. Rebates and Discounts
-                are handled following the directives of the OPEN tokenholders.
+                <ExternalLink href="https://openworld.vision/">OPEN</ExternalLink> is under no obligation for Rebates or
+                Discounts to any Referrer or Trader. Rebates and Discounts are handled following the directives of the
+                OPEN tokenholders.
               </p>
               <p className="body-text">
                 The Rebates and Discounts percentages for the default tier Tier 1, and instructions to upgrade to Tier 2
@@ -96,56 +101,60 @@ export default function ReferralTerms() {
             <div className="section">
               <h3 className="body-title">4. LIMITATION OF LIABILITY</h3>
               <p className="body-text">
-                Under no circumstances shall OPEN.io be liable for any direct, indirect, incidental, punitive, special,
-                consequential damages, or similar damages or liabilities whatsoever for any reason whatsoever related to
-                these Terms, your use or inability to use our web site(s), or the materials and content of the web
-                site(s) or any other web sites linked to such web site(s) or your provision of any personally
-                identifiable information to a backend service provider or any third party. This limitation applies
-                regardless of whether the alleged liability is based on contract, tort, warranty, negligence, strict
-                liability, or any other basis, even if we have been advised of the possibility of such damages or such
-                damages were reasonably foreseeable.
+                Under no circumstances shall <ExternalLink href="https://openworld.vision/">OPEN</ExternalLink> be
+                liable for any direct, indirect, incidental, punitive, special, consequential damages, or similar
+                damages or liabilities whatsoever for any reason whatsoever related to these Terms, your use or
+                inability to use our web site(s), or the materials and content of the web site(s) or any other web sites
+                linked to such web site(s) or your provision of any personally identifiable information to a backend
+                service provider or any third party. This limitation applies regardless of whether the alleged liability
+                is based on contract, tort, warranty, negligence, strict liability, or any other basis, even if we have
+                been advised of the possibility of such damages or such damages were reasonably foreseeable.
               </p>
               <p className="body-text">
-                In no event shall OPEN.io's aggregate liability arising out of or in connection with the Site exceed
-                five hundred Singapore Dollars (500 SGD).
+                In no event shall <ExternalLink href="https://openworld.vision/">OPEN</ExternalLink>'s aggregate
+                liability arising out of or in connection with the Site exceed five hundred Singapore Dollars (500 SGD).
               </p>
             </div>
             <div className="section">
               <h3 className="body-title">5. INDEMNIFICATION</h3>
               <p className="body-text">
-                You will defend, indemnify, and hold harmless OPEN.io, its affiliates, members, member, managers,
-                employees, attorneys, representatives, suppliers, and contractors from any claim, demand, lawsuit,
-                action, proceeding, investigation, liability, damage, loss, cost or expense, including without
+                You will defend, indemnify, and hold harmless openworld.vision, its affiliates, members, member,
+                managers, employees, attorneys, representatives, suppliers, and contractors from any claim, demand,
+                lawsuit, action, proceeding, investigation, liability, damage, loss, cost or expense, including without
                 limitation reasonable attorneys' fees, arising out of or relating to arising under these Terms, the
-                service(s) provided by OPEN.io, or your use of the service(s) provided by OPEN.io, including, without
-                limitation, an infringement by you, or by anyone else using such service(s) we provide to you, of any
-                intellectual property or other proprietary rights of any person or entity, or from the violation of any
-                of our operating rules or policies relating to the service(s) provided. When we may be involved in a
-                suit involving a third party and which is related to our service(s) to you under these Terms, we may
-                seek written assurances from you in which you promise to defend, indemnify and hold us harmless from the
-                costs and liabilities described in this paragraph. Such written assurances may include, in our sole
-                discretion, the posting of a performance bond(s) or other guarantees reasonably calculated to guarantee
-                payment. We may consider your failure to provide such assurances a breach of these Terms by you. The
-                terms of this paragraph will survive any termination or cancellation of the Terms.
+                service(s) provided by openworld.vision, or your use of the service(s) provided by openworld.vision,
+                including, without limitation, an infringement by you, or by anyone else using such service(s) we
+                provide to you, of any intellectual property or other proprietary rights of any person or entity, or
+                from the violation of any of our operating rules or policies relating to the service(s) provided. When
+                we may be involved in a suit involving a third party and which is related to our service(s) to you under
+                these Terms, we may seek written assurances from you in which you promise to defend, indemnify and hold
+                us harmless from the costs and liabilities described in this paragraph. Such written assurances may
+                include, in our sole discretion, the posting of a performance bond(s) or other guarantees reasonably
+                calculated to guarantee payment. We may consider your failure to provide such assurances a breach of
+                these Terms by you. The terms of this paragraph will survive any termination or cancellation of the
+                Terms.
               </p>
             </div>
             <div className="section">
               <h3 className="body-title">6. MODIFICATION, SUSPENSION, AND TERMINATION</h3>
               <p className="body-text">
-                The Referral Program runs independently from OPEN.io, which the token holders of OPEN determine. Due to
-                this, from time to time and with or without prior notice to you, the Referral Program could be subject
-                to modifications, suspensions, or disabilities, in whole or in part, for any reason whatsoever.
+                The Referral Program runs independently from openworld.vision, which the token holders of OPEN
+                determine. Due to this, from time to time and with or without prior notice to you, the Referral Program
+                could be subject to modifications, suspensions, or disabilities, in whole or in part, for any reason
+                whatsoever.
               </p>
               <p className="body-text">
-                OPEN.io will not be liable for any losses suffered by you resulting from any modification to the
-                Referral Program or from any modification, suspension, or termination, for any reason, of your access to
-                all or any portion of the Interface or the Protocol.
+                <ExternalLink href="https://openworld.vision/">OPEN</ExternalLink> will not be liable for any losses
+                suffered by you resulting from any modification to the Referral Program or from any modification,
+                suspension, or termination, for any reason, of your access to all or any portion of the Interface or the
+                Protocol.
               </p>
               <p className="body-text">
-                OPEN.io may revise these Terms from time to time. We will notify you by updating the date at the top of
-                the Terms and maintaining a current version. The most current version of the Terms will always be at
-                https://gmx.io/referral-terms. All modifications will be effective when they are posted. By using the
-                Referral Program after those revisions become effective, you agree to be bound by the revised Terms.
+                <ExternalLink href="https://openworld.vision/">OPEN</ExternalLink> may revise these Terms from time to
+                time. We will notify you by updating the date at the top of the Terms and maintaining a current version.
+                The most current version of the Terms will always be at https://gmx.io/referral-terms. All modifications
+                will be effective when they are posted. By using the Referral Program after those revisions become
+                effective, you agree to be bound by the revised Terms.
               </p>
             </div>
             <div className="section">
@@ -160,10 +169,11 @@ export default function ReferralTerms() {
               <h3 className="body-title">8. GENERAL</h3>
               <p className="body-text">
                 8.1 These Terms, including other policies or agreements incorporated herein, constitute the entire and
-                only agreement between you and OPEN.io concerning the subject matter of these Terms and supersede all
-                prior or contemporaneous agreements, representations, warranties, and understandings, written or oral,
-                concerning the subject matter of these Terms. Any failure by us to exercise or enforce any right or
-                provision of the Terms shall not constitute a waiver of such right or provision.
+                only agreement between you and <ExternalLink href="https://openworld.vision/">OPEN</ExternalLink>{" "}
+                concerning the subject matter of these Terms and supersede all prior or contemporaneous agreements,
+                representations, warranties, and understandings, written or oral, concerning the subject matter of these
+                Terms. Any failure by us to exercise or enforce any right or provision of the Terms shall not constitute
+                a waiver of such right or provision.
               </p>
               <p className="body-text">
                 8.2 You must legally be able to enter into the Terms. Using the Referral Program, you represent and
@@ -178,30 +188,32 @@ export default function ReferralTerms() {
               <p className="body-text">
                 8.4 If any provision of these Terms shall be unenforceable or invalid under any applicable law or be
                 held by any applicable court decision, such unenforceability or invalidity shall not render these Terms
-                unenforceable or invalid as a whole. OPEN.io will amend or replace such provision with one that is valid
-                and enforceable and which achieves, to the extent possible, our original objectives and intent as
-                reflected in the original provision.
+                unenforceable or invalid as a whole. <ExternalLink href="https://openworld.vision/">OPEN</ExternalLink>{" "}
+                will amend or replace such provision with one that is valid and enforceable and which achieves, to the
+                extent possible, our original objectives and intent as reflected in the original provision.
               </p>
               <p className="body-text">
                 8.5 You may not assign or transfer any right to use the Referral Program, or any of your rights or
                 obligations under these Terms, without our express prior written consent, including by operation of law
-                or in connection with any change of control. OPEN.io may assign or transfer any or all of our rights or
-                obligations under these Terms, in whole or part, without notice or obtaining your consent or approval.
+                or in connection with any change of control.{" "}
+                <ExternalLink href="https://openworld.vision/">OPEN</ExternalLink> may assign or transfer any or all of
+                our rights or obligations under these Terms, in whole or part, without notice or obtaining your consent
+                or approval.
               </p>
               <p className="body-text">
-                8.6 OPEN.io will have no responsibility or liability for any failure or delay in performance of the
-                Site, or any loss or damage that you may incur, due to any circumstance or event beyond our control,
-                including without limitation any flood, extraordinary weather conditions, earthquake, or other act of
-                God, fire, war, insurrection, riot, labor dispute, accident, any law, order regulation, direction,
-                action or request of the government, communications, power failure, or equipment or software
-                malfunction.
+                8.6 <ExternalLink href="https://openworld.vision/">OPEN</ExternalLink> will have no responsibility or
+                liability for any failure or delay in performance of the Site, or any loss or damage that you may incur,
+                due to any circumstance or event beyond our control, including without limitation any flood,
+                extraordinary weather conditions, earthquake, or other act of God, fire, war, insurrection, riot, labor
+                dispute, accident, any law, order regulation, direction, action or request of the government,
+                communications, power failure, or equipment or software malfunction.
               </p>
             </div>
             <div className="section">
               <h3 className="body-title">9. CONTACT INFORMATION</h3>
               <p className="body-text">
-                If you have any questions about these Terms, the Site, or the Interface, please get in touch with
-                OPEN.io on any of our official channels.
+                If you have any questions about these Terms, the Site, or the Interface, please get in touch with{" "}
+                <ExternalLink href="https://openworld.vision/">OPEN</ExternalLink> on any of our official channels.
               </p>
             </div>
           </div>
